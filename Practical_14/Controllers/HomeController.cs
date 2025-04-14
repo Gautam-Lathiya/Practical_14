d9b1d7322e598f1421de52c7b0472d455c53b61d
Gautam-Lathiya <gautamlathiya18@gmail.com>
2025-04-14 23:21:14 +0530

    Implement Search functionality

--- Practical_14/Controllers/HomeController.cs
+++ Practical_14/Controllers/HomeController.cs
@@ -13,18 +13,5 @@ namespace Practical_14.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
